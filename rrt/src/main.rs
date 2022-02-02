79e7ec2a0a732697740fde0cabe589dc4b8bdc29
Hugh Giddens <hgiddens@gmail.com>
2022-02-02 15:12:26 +0100

    Chapter 1 completed.

--- rrt/src/main.rs
+++ rrt/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let cols = 200;
+    let rows = 100;
+    println!("P3");
+    println!("{} {}", cols, rows);
+    println!("255");
+
+    for row in (0..rows).rev() {
+        for col in 0..cols {
+            let r = row as f32 / rows as f32;
+            let g = col as f32 / cols as f32;
+            let b = 0.2f32;
+
+            let ir = (255.9 * r) as i32;
+            let ig = (255.9 * g) as i32;
+            let ib = (255.9 * b) as i32;
+
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
 }
