f92726957e4d0c527faf36faae46b96809af569e
Hugh Giddens <hgiddens@gmail.com>
2022-02-02 17:04:27 +0100

    Chapter 3.
    More stuff deferred.

--- rrt/src/colour.rs
+++ rrt/src/colour.rs
@@ -1,18 +1,42 @@
-#[derive(Debug)]
+#[derive(Clone, Copy, Debug)]
 pub struct Colour {
     r: f32,
     g: f32,
-    b: f32
+    b: f32,
 }
 
 impl Colour {
-    pub fn new(r: f32, g: f32, b: f32) -> Self {
-        Colour{ r: r, g: g, b: b }
+    // TODO: Way of constraining f to be [0, 1]?
+    pub fn blend(from: Colour, to: Colour, t: f32) -> Self {
+        let u = 1.0 - t;
+        Colour {
+            r: u * from.r + t * to.r,
+            g: u * from.g + t * to.g,
+            b: u * from.b + t * to.b,
+        }
     }
-    
+
+    pub fn white() -> Self {
+        Colour {
+            r: 1.0,
+            g: 1.0,
+            b: 1.0,
+        }
+    }
+
+    pub fn light_sky_blue() -> Self {
+        Colour {
+            r: 0.5294118,
+            g: 0.80784315,
+            b: 0.98039216,
+        }
+    }
+
     pub fn rgb8(self) -> (u8, u8, u8) {
-        ((self.r * 255.9f32) as u8,
-         (self.g * 255.9f32) as u8,
-         (self.b * 255.9f32) as u8)
+        (
+            (self.r * 255.9f32) as u8,
+            (self.g * 255.9f32) as u8,
+            (self.b * 255.9f32) as u8,
+        )
     }
 }
--- rrt/src/main.rs
+++ rrt/src/main.rs
@@ -1,5 +1,18 @@
 mod colour;
+mod pt3;
+mod ray;
+mod vec3;
+
 use crate::colour::Colour;
+use crate::pt3::Pt3;
+use crate::ray::Ray;
+use crate::vec3::Vec3;
+
+fn ray_colour(ray: Ray) -> Colour {
+    let unit_direction = ray.direction().unit_vector();
+    let t = (unit_direction.y + 1.0) * 0.5;
+    Colour::blend(Colour::white(), Colour::light_sky_blue(), t)
+}
 
 fn main() {
     let cols = 200;
@@ -8,13 +21,20 @@ fn main() {
     println!("{} {}", cols, rows);
     println!("255");
 
+    let lower_left_corner = Pt3 {
+        x: -2.0,
+        y: -1.0,
+        z: -1.0,
+    };
+    let horizontal = Vec3::new(4.0, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, 2.0, 0.0);
+
     for row in (0..rows).rev() {
         for col in 0..cols {
-            let colour = Colour::new(
-                row as f32 / rows as f32,
-                col as f32 / cols as f32,
-                0.2
-            );
+            let u = col as f32 / cols as f32;
+            let v = row as f32 / rows as f32;
+            let ray = Ray::from_origin_to(lower_left_corner + u * horizontal + v * vertical);
+            let colour = ray_colour(ray);
             let (ir, ig, ib) = colour.rgb8();
             println!("{} {} {}", ir, ig, ib);
         }
