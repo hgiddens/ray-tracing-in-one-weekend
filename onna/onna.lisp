c5ef4f3769d03e1c78d599086a71f744af9eac0c
Hugh Giddens <hgiddens@gmail.com>
2025-01-01 14:47:09 +0100

    Chapter 3.

--- onna/onna.lisp
+++ onna/onna.lisp
@@ -9,14 +9,68 @@
   (flet ((8bit (x) (floor (* x 255.99))))
     (list (8bit (colour-r c)) (8bit (colour-g c)) (8bit (colour-b c)))))
 
+(defstruct (vec (:constructor make-vec (x y z)))
+  (x 0 :type real)
+  (y 0 :type real)
+  (z 0 :type real))
+
+(defun scaled-vec (v n)
+  "Returns a new VEC corresponding to V scaled by N"
+  (with-slots (x y z) v
+    (make-vec (* x n) (* y n) (* z n))))
+
+(defun summed-vecs (&rest vs)
+  "Returns a new VEC representing the sum of VS"
+  (let ((s (make-vec 0 0 0)))
+    (with-slots (x y z) s
+      (dolist (v vs s)
+        (incf x (vec-x v))
+        (incf y (vec-y v))
+        (incf z (vec-z v))))))
+
+(defun vec-length (v)
+  (with-slots (x y z) v
+    (sqrt (+ (* x x) (* y y) (* z z)))))
+
+(defun unit-vec (v)
+  "Returns a new VEC representing V scaled to a unit length"
+  (with-slots (x y z) v
+    (let ((l (vec-length v)))
+      (make-vec (/ x l) (/ y l) (/ z l)))))
+
+(defstruct (point (:constructor make-point (x y z)) (:include vec)))
+
+(defun offset-point (p v)
+  "Returns a new POINT offset from P by V"
+  (with-slots (px py pz) p
+    (with-slots (vx vy vz) v
+      (make-point (+ px vx) (+ py vy) (+ pz vz)))))
+
+(defstruct ray
+  (origin (make-point 0 0 0) :type point)
+  (direction (make-vec 0 0 0) :type vec))
+
+(defun ray-colour (r)
+  (flet ((lerp (start end n)
+           (+ (* (- 1 n) start) (* n end))))
+    (let* ((unit-direction (unit-vec (ray-direction r)))
+           (n (* 0.5 (1+ (vec-y unit-direction)))))
+      (make-colour (lerp 1 0.5 n) (lerp 1 0.7 n) 1))))
+
 (defun test-image (nx ny)
-  (let ((a (make-array (list ny nx) :element-type 'colour :initial-element (make-colour 0 0 0))))
+  (let ((a (make-array (list ny nx) :element-type 'colour :initial-element (make-colour 0 0 0)))
+        (lower-left-corner (make-vec -2 -1 -1))
+        (horizontal (make-vec 4 0 0))
+        (vertical (make-vec 0 2 0))
+        (origin (make-point 0 0 0)))
     (loop for j below ny do
       (loop for i below nx do
-        (let ((r (/ i nx))
-              (g (/ j ny))
-              (b 0.2))
-        (setf (aref a j i) (make-colour r g b)))))
+        (let* ((u (/ i nx))
+               (v (/ j ny))
+               (r (make-ray :origin origin :direction (summed-vecs lower-left-corner
+                                                                   (scaled-vec horizontal u)
+                                                                   (scaled-vec vertical v)))))
+          (setf (aref a j i) (ray-colour r)))))
     a))
 
 (defun write-image (image)
