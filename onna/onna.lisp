15a8bd5abf4d4e317417ae4cb91107132f58b37a
Hugh Giddens <hgiddens@gmail.com>
2025-04-01 01:42:04 +0200

    [Common Lisp] Book 1, chapter 14.

--- onna/onna.lisp
+++ onna/onna.lisp
@@ -4,13 +4,14 @@
   "Example function rendering whatever I'm working on."
   ;; Not adding a progress bar, because it's not clear to me how I can do that
   ;; well with this being run primarily from Slime.
-  (let* ((camera (make-camera :samples-per-pixel 100
+  (let* ((camera (make-camera :samples-per-pixel 10
+                              :image-width 600
                               :max-depth 50
                               :vertical-fov 20
-                              :look-from (make-point3 -2 2 1)
-                              :defocus-angle 10
-                              :focus-distance 3.4))
-         (world (four-spheres))
+                              :look-from (make-point3 13 2 3)
+                              :defocus-angle 0.6
+                              :focus-distance 10))
+         (world (book-1-cover))
          (image (time (render camera world))))
     (with-open-file (stream #P"~/Desktop/test.ppm"
                             :direction :output
--- onna/scenes.lisp
+++ onna/scenes.lisp
@@ -28,3 +28,56 @@
    (make-sphere :centre (make-point3 1 0 -1)
                 :radius 0.5
                 :material (make-metal :albedo (make-colour 0.8 0.6 0.2) :fuzz 1d0))))
+
+(defun book-1-cover ()
+  "Cover image of the first book."
+  (flet ((random-material ()
+           (let ((choose-material (random 1.0)))
+             (cond
+               ((< choose-material 0.8)
+                (make-lambertian :albedo (make-colour (* (random 1.0) (random 1.0))
+                                                      (* (random 1.0) (random 1.0))
+                                                      (* (random 1.0) (random 1.0)))))
+               ((< choose-material 0.95)
+                (make-metal :albedo (make-colour (+ 0.5 (random 0.5))
+                                                 (+ 0.5 (random 0.5))
+                                                 (+ 0.5 (random 0.5)))
+                            :fuzz (random 0.5d0)))
+               (t
+                (make-dielectric :refraction-index 1.5d0))))))
+    (let (world)
+      ;; Ground
+      (push (make-sphere :centre (make-point3 0 -1000 0)
+                         :radius 1000
+                         :material (make-lambertian :albedo (make-colour 0.5 0.5 0.5)))
+            world)
+
+      ;; Thousands of luminous spheres
+      (loop with exclusion-centre = (make-point3 4 0.2 0)
+            for a from -11 below 11
+            nconcing (loop for b from -11 below 11
+                           as x = (+ a (random 0.9d0))
+                           as z = (+ b (random 0.9d0))
+                           as sphere-centre = (make-point3 x 0.2 z)
+                           when (> (vec3-length (point3- sphere-centre exclusion-centre)) 0.9)
+                             collect (make-sphere :centre sphere-centre
+                                                  :radius 0.2
+                                                  :material (random-material)))
+              into little-spheres
+            finally (alexandria:nconcf world little-spheres))
+
+      ;; Big spheres
+      (push (make-sphere :centre (make-point3 0 1 0)
+                         :radius 1
+                         :material (make-dielectric :refraction-index 1.5d0))
+            world)
+      (push (make-sphere :centre (make-point3 -4 1 0)
+                         :radius 1
+                         :material (make-lambertian :albedo (make-colour 0.4 0.2 0.1)))
+            world)
+      (push (make-sphere :centre (make-point3 4 1 0)
+                         :radius 1
+                         :material (make-metal :albedo (make-colour 0.7 0.6 0.5)))
+            world)
+
+      (coerce world 'vector))))
